d2d18e71a8f1ebe94be003c166944ea09dcac3fe
Jason27k <68726620+Jason27k@users.noreply.github.com>
2024-10-20 10:18:14 -0700

    prep for dev env

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { fetchTopAnime } from "../actions";
+import AnimeDisplay from "@/components/AnimeDisplay";
 
-const page = () => {
-  return <div>page</div>;
+const page = async () => {
+  const top = await fetchTopAnime(1, undefined);
+  const topAnime = top?.data;
+
+  if (!topAnime) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>{/* <AnimeDisplay anime */}</div>;
 };
 
 export default page;
